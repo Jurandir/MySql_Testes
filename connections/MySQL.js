63dd521e2fee25d3ba4b00db5b9aa447f2e11b00
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2021-03-31 08:54:53 -0300

    Ajustes / BUG MySQL connector

--- connections/MySQL.js
+++ connections/MySQL.js
@@ -23,8 +23,13 @@ const MySQL = {
       return 0
   },
   sqlQueryDB: async function (sel,par) {
-      const [rows, fields] = await global.connection.execute(sel,par)  
-      return rows  
+      try {
+        const [rows, fields] = await global.connection.execute(sel,par)  
+        return rows  
+      } catch (err) {
+          console.log('Err:',err)
+          return []
+      }
   }
 }
 
